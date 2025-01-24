6ede91b274f063df75b6955b50049293601a523e
shelleylwang <seashellsw@gmail.com>
2025-01-24 10:23:10 -0500

    creating final reusable hespdiv_functions script, testing study area polygon in hespdiv.r

--- hespdiv.r
+++ hespdiv.r
@@ -6,6 +6,21 @@ library(sp)
 
 devtools::install_github("Liudas-Dau/hespdiv_data")
 library(HDData)
+
+study_area_polygon <- str(us)
+View(us)
+plot(us$x, us$y, type = "l", xlab = "Longitude", ylab = "Latitude", main = "Shape of 'us'")
+# ^ example study area polygon is the shape of the US
+
+# Test creating a rectangular polygon
+# Coordinates of the four corners of the rectangle
+x_coords <- c(-90, 155, 155, -90, -90)
+y_coords <- c(-80, -80, 80, 80, -80)
+rectangle_df <- data.frame(x = x_coords, y = y_coords)
+print(rectangle_df)
+plot(rectangle_df$x, rectangle_df$y, type = "l", xlab = "Longitude", ylab = "Latitude", main = "Rectangle Shape")
+
+
 mio_mams
 
 mio_mams <- data.frame(mio_mams)
@@ -17,7 +32,7 @@ unique(species)
 
 sp_coords <- data.frame(x = mio_mams$lng, y = mio_mams$lat) # Coordinates of observations
 
-str(us) # Tutorial provides a polygon representing the study 
+str(us) # Tutorial provides a polygon representing the study
 # area as a ref point when interpreting results (shows up as background in plot)
 
 # The 'hd' object exists pre-calculated in HDData library, so don't need to run this code:
@@ -54,7 +69,7 @@ plot(gr)
 
 # The 'nl' object exists pre-calculated in HDData library, so don't need to run this code:
 set.seed(1) # The seed is used to obtain the same result of an experiment with random properties.
-nl <- nulltest(hd, n = 1000) 
+nl <- nulltest(hd, n = 1000)
 plot(n1)
 
 
@@ -80,66 +95,66 @@ tb_1 <- read.csv('../BDNN_Arielli/data/hespdiv/hespdiv_bin1.csv')
 process_species_areas <- function(species_data) {
   # 1. Format data for hespdiv
   species <- species_data$genus
-  coords <- data.frame(x = species_data$'Rotated.Lon', 
+  coords <- data.frame(x = species_data$'Rotated.Lon',
                        y = species_data$'Rotated.Lat')
-  
+
   # 2. Run hespdiv
-  result <- hespdiv(data = species, 
+  result <- hespdiv(data = species,
                     xy.dat = coords)
-  
+
   # 3. Function to find all polygons containing a point
   find_all_polygons <- function(x, y, result) {
     point <- c(x, y)
     polygons <- result$polygons.xy
     ranks <- result$poly.stats$rank
-    
+
     # Check each polygon
     contains_point <- logical(length(polygons))
-    
+
     for(i in seq_along(polygons)) {
       poly <- polygons[[i]]
       # point.in.polygon returns 0 if point is outside, 1 if inside, 2 if on boundary
-      inside <- point.in.polygon(point[1], point[2], 
+      inside <- point.in.polygon(point[1], point[2],
                                  poly$x, poly$y) > 0
       contains_point[i] <- inside
     }
-    
+
     return(contains_point)
   }
-  
+
   # 4. Apply to all species and create columns for each rank
   # First get all polygon assignments
-  polygon_assignments <- mapply(find_all_polygons, 
-                                species_data$'Rotated.Lon', 
+  polygon_assignments <- mapply(find_all_polygons,
+                                species_data$'Rotated.Lon',
                                 species_data$'Rotated.Lat',
                                 MoreArgs = list(result = result))
-  
+
   # Transpose to get matrix where rows are species and columns are polygons
   polygon_assignments <- t(polygon_assignments)
-  
+
   # Create new columns for each rank
   max_rank <- max(result$poly.stats$rank)
-  
+
   # Initialize new columns in species_data for each rank
   for(rank in 1:max_rank) {
     col_name <- paste0("area_rank_", rank)
     species_data[[col_name]] <- NA
   }
-  
+
   # For each species (row)
   for(i in 1:nrow(species_data)) {
     # Get polygons this species belongs to
     species_polygons <- which(polygon_assignments[i,])
-    
+
     if(length(species_polygons) > 0) {
       # Get ranks for these polygons
       polygon_ranks <- result$poly.stats$rank[species_polygons]
-      
+
       # For each rank present
       for(rank in unique(polygon_ranks)) {
         # Get polygon ID for this rank
         rank_polygons <- species_polygons[polygon_ranks == rank]
-        
+
         if(length(rank_polygons) > 0) {
           col_name <- paste0("area_rank_", rank)
           species_data[i, col_name] <- paste(rank_polygons, collapse = ";")
@@ -147,10 +162,10 @@ process_species_areas <- function(species_data) {
       }
     }
   }
-  
+
   # Add polygon statistics to help interpret the results
   attr(species_data, "polygon_stats") <- result$poly.stats
-  
+
   return(species_data)
 }
 
@@ -159,23 +174,23 @@ process_species_areas <- function(species_data) {
 # Visualization function
 plot_species_areas <- function(result_data, rank = 1) {
   col_name <- paste0("area_rank_", rank)
-  
+
   if(!(col_name %in% colnames(result_data))) {
     stop(paste("No data for rank", rank))
   }
-  
+
   # Plot base map
   plot(result_data$'Rotated.Lon', result_data$'Rotated.Lat',
-       type = "n", 
-       xlab = "Longitude", 
+       type = "n",
+       xlab = "Longitude",
        ylab = "Latitude",
        main = paste("Species Distribution by Area, Rank", rank))
-  
+
   # Add points colored by area
   areas <- unique(result_data[[col_name]])
   areas <- areas[!is.na(areas)]
   colors <- rainbow(length(areas))
-  
+
   for(i in seq_along(areas)) {
     area_species <- result_data[[col_name]] == areas[i]
     points(result_data$'Rotated.Lon'[area_species],
@@ -183,9 +198,9 @@ plot_species_areas <- function(result_data, rank = 1) {
            col = colors[i],
            pch = 19)
   }
-  
+
   # Add legend
-  legend("topright", 
+  legend("topright",
          legend = areas,
          col = colors,
          pch = 19,
---
+++ hespdiv_functions.r
@@ -0,0 +1,9 @@
+# Install and load required packages
+install.packages("devtools")
+devtools::install_github("Liudas-Dau/hespdiv")
+library(hespdiv)
+install.packages("sp")
+library(sp)
+setwd('C:/Users/SimoesLabAdmin/Documents/DeepDiveR')
+
+### LOAD DATA +
